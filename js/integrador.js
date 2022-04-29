0fc88fa2318a31529b0ea090bbfe4b34db54bce3
andressebastiandiz <andres.diz@bue.edu.ar>
2022-04-29 12:08:20 -0300

    first commit

---
+++ js/integrador.js
@@ -0,0 +1,41 @@
+
+function resalta() {
+    document.getElementById("button1").style.backgroundColor="red";
+    
+}
+
+function opar() {
+    document.getElementById("button1").style.background="none";
+    
+}
+
+function resalto() {
+    document.getElementById("button2").style.color="yellow";
+    
+}
+
+function opac() {
+    document.getElementById("button2").style.color="white"
+    
+}
+
+
+function resaltar() {
+    document.getElementById("button3").style.backgroundColor="red";
+    
+}
+
+function opacar() {
+    document.getElementById("button3").style.backgroundColor="rgb(53, 58, 64)"
+    
+}
+
+function resalt() {
+    document.getElementById("button4").style.color="yellow"
+    
+}
+
+function op() {
+    document.getElementById("button4").style.color="white";
+    
+}
